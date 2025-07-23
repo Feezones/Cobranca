5945fd4dedc3a27789b7d6223e267cf49cabbdfc
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2025-07-23 12:00:03 -0300

    gitignore

--- obj/Debug/net9.0/FitBack.AssemblyInfo.cs
+++ obj/Debug/net9.0/FitBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd77f2a81fcbce2148d04c3dfe544d3d31619522")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a462b529fcef8b71796db22bc5a41ef6812c4d0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
